69620e1ad69dfc892367334ea2030afce92a74cd
Daniel Moscoe <dmoscoe@gmail.com>
2020-06-18 19:49:03 -0400

    Date and Time done. Still working on ch 3.

---
+++ pre02/Date.java
@@ -0,0 +1,11 @@
+public class Date {
+	
+	public static void main(String[] args) {
+		String day = "Thursday";
+		int date = 18;
+		System.out.println("American format:");
+		System.out.println(day + ", June " + date + ", 2020");
+		System.out.println("European format:");
+		System.out.println(day +" " + date + " " + "June 2020");
+	}
+}
