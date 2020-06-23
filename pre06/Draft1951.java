ff349320bea826c5b6a039c29f56d903fd762ff5
Daniel Moscoe <dmoscoe@gmail.com>
2020-06-22 21:06:24 -0400

    Draft in progress...

--- pre06/Draft1951.java
+++ pre06/Draft1951.java
@@ -14,7 +14,19 @@ public class Draft1951 {
 			y -= 1;
 		} return factorial;
 	}
-
+	
+	/**
+	myexp works for small x, n, but something goes weird when n gets large. For x = 5 and x = 7 this happens when n > 12. 
+	*/
+	public static float myexp(int x, int n) {
+		float myexp = 1;
+		while (n > 0) {
+			myexp += power(x,n)/factorial(n);
+			System.out.println("next term is " + power(x,n)/factorial(n) + ", n = " + n + ", myexp = " + myexp);
+			n -= 1;
+			
+		} return myexp;
+	}
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -27,6 +39,12 @@ public class Draft1951 {
 		System.out.print("Enter the arg for factorial: ");
 		int arg = in.nextInt();
 		System.out.println(arg + "! = " + factorial(arg));
+		
+		System.out.print("Enter the tiny on e: ");
+		int tiny = in.nextInt();
+		System.out.print("Enter the number of terms: ");
+		int terms = in.nextInt();
+		System.out.println("e^" + tiny + " is approx. equal to " + myexp(tiny, terms));
 	}
 }
 		
