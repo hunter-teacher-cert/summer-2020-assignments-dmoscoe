3ac87d52a9c91138e9fccc49610a372c7698cc51
Daniel Moscoe <dmoscoe@gmail.com>
2020-07-03 14:40:12 -0400

    Create Sample.java

---
+++ classwork/200703/Sample.java
@@ -0,0 +1,6 @@
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("This is the first line.");
+		System.out.println("This is the second line that I will delete.");
+	}
+}
