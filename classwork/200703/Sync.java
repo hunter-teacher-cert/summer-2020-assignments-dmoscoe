80aad0b050078b4205f0dd3303215a0806a0ea31
Daniel Moscoe <dmoscoe@gmail.com>
2020-07-03 16:01:04 -0400

    Delete Sync.java

--- classwork/200703/Sync.java
+++
@@ -1,6 +0,0 @@
-public class Sync {
-	public static void Main(String[] args) {
-		System.out.println("I would like to sync files with my laptop. Is this going to create a nightmare?");
-		System.out.println("Now I'm changing the file on my laptop. Let's see what happens.");
-	}
-}
