721372cb25b886bcaa93803f51a31f557637fee3
Daniel Moscoe <dmoscoe@gmail.com>
2020-07-03 14:43:27 -0400

    Delete Sample.java

--- classwork/200703/Sample.java
+++
@@ -1,6 +0,0 @@
-public class Sample {
-	public static void main(String[] args) {
-		System.out.println("This is the first line.");
-		System.out.println("This is the third line that I added. I also deleted the second line.");
-	}
-}
