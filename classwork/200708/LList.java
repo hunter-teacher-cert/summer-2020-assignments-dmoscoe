d09b0e4d7a19b15dd01cde4e06a0a5f1881372d6
Daniel Moscoe <dmoscoe@gmail.com>
2020-07-11 17:23:33 -0400

    added exceptions

--- classwork/200708/LList.java
+++ classwork/200708/LList.java
@@ -1,26 +1,32 @@
-// 200711: Replaced the method length() with just a getter. Length now gets incremented live every time a node is added or removed. Added testing for length to the Driver file. Added nodeAt and refactored other methods.
+/* lingering questions:
+
+ Why do we need to declare instance variables at the top of the class and not in the constructor? I guess I'm confused about the sequence of operations when a new LList is declared. Do we first look at public class LList, or do we first look at public LList() ?
+ 
+ When an exception is thrown, it derails the entire program (unless it's caught?). But this makes the driver less useful, because it stops testing as soon as it encounters an exception. So I guess this means I have to catch the exceptions, right? */
 
 import java.io.*;
 import java.util.*;
 
 public class LList {
-	private Node head; //a linked list is defined by the node at the head of the list.
-	private int length; //Why does length get initialized here and not in public LList() ?
+	private Node head;
+	private int length;
 	
-	public LList() { //is this the only constructor we need? We don't need a value constructor?
+	public LList() {
 		head = null;
 		length = 0;
 	}
 	
 	public void add(String data) {
 		Node newnode = new Node(data);
-		newnode.setNext(head); //first point the new node to the head of the LList.
-		head = newnode; //point head to the new node.
+		newnode.setNext(head);
+		head = newnode;
 		length++;
 	}
 	
-	public Node nodeAt(int index) { //returns a node at a given index.
-		if (index < length) {
+	public Node nodeAt(int index) {
+		if (index == 0) {
+			return head;
+		} else if (0 < index && index < length) {
 			int i = index;
 			Node tmp;
 			tmp = head;
@@ -30,23 +36,13 @@ public class LList {
 			}
 			return tmp;
 		} else {
-			return null; // replace with exception
+			
+			throw new IndexOutOfBoundsException();
+			
 		}
 	}
-	
-/* 	public String toString() {
-		String s = "";
-		Node tmp;
-		tmp = head;
-		while (tmp != null) {
-			s = s + tmp.getData() + "-->"; //Now we need to move tmp to the next node. This is the magic linked list idiom! It moves the pointer to the next node! It's analogous to i++ for an array (for loop).
-			tmp = tmp.getNext();
-		}
-		s = s + "null"; //indicates that you've reached the end of the LList.
-		return s;
-	} */
-	
-	public String toString() { //utilizes nodeAt().
+
+	public String toString() {
 		String s = "";
 		int i = 0;
 		while (i < length) {
@@ -56,107 +52,62 @@ public class LList {
 		return s;
 	}
 
-	public void addFront(String value) { //we actually already had this with add(String data) above.
+	public void addFront(String value) {
+		
+		if (value == null) {
+			
+			throw new IllegalArgumentException();
+		
+		}
 		Node newnode = new Node(value);
 		newnode.setNext(head);
 		head = newnode;
 		length++;
+		
 	}
 	
 	public boolean isEmpty() {
 		return (head == null);
 	}
 	
-	public int length() { //needed this to check whether index is out of bounds in get(int index).
+	public int length() {
 		return length;
 	}
 	
-/* 	public String get(int index) {
-		if (index < length) {
-			int i = index;
-			Node tmp;
-			tmp = head;
-			while (i > 0) {
-				tmp = tmp.getNext();
-				i--;
-			}
-			return tmp.getData();
-		} else {
-			return null;
+	public String get(int index) {
+		if (0 <= index && index < length) {
+			return this.nodeAt(index).getData();
+		} else{
+			
+			throw new IndexOutOfBoundsException();
+			
 		}
-	} */
-	
-	public String get(int index) { //utilizes nodeAt().
-		return this.nodeAt(index).getData(); //include an exception here for when index is greater than length. Or do you include the exception back at nodeAt()? I think you do it here...
 	}
 	
-/* 	public void set(int index, String value) {
-		if (index < length) {
-			int i = index;
-			Node tmp;
-			tmp = head;
-			while (i > 0) {
-				tmp = tmp.getNext();
-				i--;
-			}
-			tmp.setData(value);
-		}
-	} */
-	
-	public void set(int index, String value) { //utilizes nodeAt().
+	public void set(int index, String value) {
 		this.nodeAt(index).setData(value);
 	}
 	
-/* 	public void insert(int index, String value) {
-		//plan: if index == 0 then just use addFront. If index > 0 and valid, then make a new node, inserter(value). Traverse the LList using tmp to index - 1. Set inserter's next to be tmp's next. And set tmp's next to be inserter.
+	public void insert(int index, String value) {
 		if (index == 0) {
 			this.addFront(value);
-		} else {
-			if (index <= length) {
-				int counter = 1;
-				Node inserter = new Node(value);
-				Node tmp;
-				tmp = head;
-				while (counter < index) {
-					tmp = tmp.getNext();
-					counter++;
-				}
-				inserter.setNext(tmp.getNext());
-				tmp.setNext(inserter);
-				length++;
-			}
-		}
-	} */
-	
-	public void insert(int index, String value) { //utilizes nodeAt().
-		if (index == 0) {
-			this.addFront(value);
-		} else {
-			if (index <= length) {
+		} else if (index < length) {
 				Node inserter = new Node(value);
 				inserter.setNext(this.nodeAt(index));
 				this.nodeAt(index - 1).setNext(inserter);
 				length++;
-			}
+			} else if (index == length) {
+				Node inserter = new Node(value);
+				this.nodeAt(index - 1).setNext(inserter);
+				length++;			
+			} else if (index > length) {
+				
+				throw new IndexOutOfBoundsException();
+				
 		}
 	}
 	
-/* 	public int search(String key) {
-		//plan: traverse the LList. At each node, ask whether getData() equals key. If yes, return the index. If index == length, return -1.
-		int i = 0;
-		Node tmp;
-		tmp = head;
-		while (i < length) {
-			if (key.equals(tmp.getData())) {
-				return i;//stop after you've found the first instance of the search term.
-			}
-			tmp = tmp.getNext();
-			i++;
-		}
-		return -1;
-	} */
-	
-	public int search(String key) { //utilizes nodeAt().
+	public int search(String key) {
 		int i = 0;
 		while (i < length) {
 			if (key.equals(this.nodeAt(i).getData())) {
@@ -164,41 +115,25 @@ public class LList {
 			}
 			i++;
 		}
-		return -1;
+		
+		throw new NoSuchElementException();
+		
 	}
 	
-/* 	public void remove(int index) {
-		//plan: make a tmp node. Traverse to index - 1. set this node's Next to the next of its successor. (Skip a node.)
-		if (index == 0) { //be- and re-heading the LList.
-			head = head.getNext();
-			length--;
-		} else {
-			if (index <= length) {
-				int i = 1;
-				Node tmp;
-				tmp = head;
-				while (i < index) {
-					tmp = tmp.getNext();
-					i++;
-				}
-			tmp.setNext(tmp.getNext().getNext());
-			length--;
-			}
-		}
-	} */
-	
-	public void remove(int index) { //utilizes nodeAt()
+	public void remove(int index) {
 		if (index == 0) {
 			head = head.getNext();
 			length--;
-		} else if (index < length - 1) { //go to the node preceding index, and set it's next to the successor of its successor. To remove the last node, replace the penultimate node's next with null.
+		} else if (index < length - 1) {
 			this.nodeAt(index - 1).setNext(this.nodeAt(index + 1));
 			length--;
 		} else if (index == length - 1) {
 			this.nodeAt(index - 1).setNext(null);
 			length--;
+		} else if (index < 0 || index >= length) {
+			
+			throw new IndexOutOfBoundsException();
+		
 		}
 	}
-		
-	
-}//end class
+}
--- classwork/200708/LListDriver.java
+++ classwork/200708/LListDriver.java
@@ -25,13 +25,13 @@ public class LListDriver {
    System.out.println("\nTest get 0, 1, 5:");
    System.out.println(g.get(0));
    System.out.println(g.get(1));
-   System.out.println(g.get(5));
+   System.out.println(g.get(5)); //returns an exception
 
 
    System.out.println("\nTest set 0, 1, 5:");
    g.set(0, "me!");
    g.set(1, "job");
-   g.set(5, "wow");
+   g.set(5, "wow"); //returns an exception
    System.out.println(g);
 
    System.out.println("\nTest insert 0, 2, 5, 10:");
@@ -41,13 +41,13 @@ public class LListDriver {
    System.out.println("Does g.length() = 5? g.length() = " + g.length());
    g.insert(5, "cool");
    System.out.println("Does g.length() = 6? g.length() = " + g.length());
-   g.insert(10, "too far");
+   g.insert(10, "too far"); //returns an exception
    System.out.println(g);
 
    System.out.println("\nTest search \"woo!\", \"cool\", \"too far\"");
    System.out.println(g.search("woo!"));
    System.out.println(g.search("cool"));
-   System.out.println(g.search("too far"));
+   System.out.println(g.search("too far")); //returns an exception
 
    System.out.println("\nTest remove 0, 2:");
    g.remove(0);
